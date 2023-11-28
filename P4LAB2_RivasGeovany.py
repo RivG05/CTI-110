a6a31b4788fef59ca45220e4b9d82ebaa67af567
RivG05 <150169466+RivG05@users.noreply.github.com>
2023-11-28 11:10:18 -0500

    Ninth Upload

---
+++ P4LAB2_RivasGeovany.py
@@ -0,0 +1,22 @@
+#Geovany Rivas
+#11/28/2023
+#Use a for loop with the range function
+
+#Get input from the user
+num1 = int(input("Enter an integer: "))
+num2 = int(input("Enter an integer higher than the first: "))
+
+#If the first number is smaller
+if num1 < num2:
+    for i in range (num1, num2 + 1, 5):
+        print(i)
+else:
+    #While the input is invalid
+    while num1 > num2 or num1 == num2:
+        print("First number must be smaller")
+
+        #Get input from the user
+        num1 = int(input("Enter an integer: "))
+        num2 = int(input("Enter an integer higher than the first: "))
+    for i in range (num1, num2 + 1, 5):
+        print(i)
